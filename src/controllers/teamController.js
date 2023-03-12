57417b0f8f275e82631278a3df92138b00bd26b6
SigmaGrindset <antoniobnoni@gmail.com>
2023-03-12 13:26:10 +0100

    menu

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -1,4 +1,5 @@
 const Team = require("../models/Team");
+const Game = require("../models/Game");
 const TeamCurrentRoster = require("../models/TeamCurrentRoster");
 const logger = require("../config/logger");
 
@@ -6,8 +7,14 @@ module.exports.teamdetails_get = async (req, res) => {
   const teamId = req.params.teamId;
   const team = await Team.findOne({ _id: teamId });
   const teamPlayers = await TeamCurrentRoster.find({ team: team.id });
+
+  const games = await Game
+    .find({
+      $or: [{ awayTeam: team._id }, { homeTeam: team._id }]
+    })
+    .sort({ date: 1 });
   if (team) {
-    res.render("team_details.ejs", { team, teamPlayers })
+    res.render("team_details.ejs", { team, teamPlayers, games })
   } else {
     res.render("errors/error.ejs", { error: { name: "Error 404 not found", desc: "The resource you requested doesn't exist." } })
   }
