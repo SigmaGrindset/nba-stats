12d9a5c04f13306e7931e243d0d6791f542664a6
SigmaGrindset <antoniobnoni@gmail.com>
2023-03-15 22:04:30 +0100

    dsanjk

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -1,3 +1,4 @@
+
 /// <reference types="cypress" />
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -5,9 +6,25 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-
-
+const mongoose = require("mongoose");
+const Team = require("../../../src/models/Team");
+mongoose.connect("mongodb+srv://sanji:diablejambe@nba-stats.9dwaife.mongodb.net/nba-stats?retryWrites=true&w=majority")
+  .then(() => {
+    console.log(`connected to db for tests`);
+  })
+  .catch(err => { console.log(err) });
 
 module.exports = (on, config) => {
+  // on("before:spec", function () {
+  // }),
+
+
+  on("task", {
+    async getTeams({ project }) {
+      const teams = await Team.find(null, project);
+      return teams;
+    }
+  })
+
 
 }
