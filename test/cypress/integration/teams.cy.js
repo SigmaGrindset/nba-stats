511ba957f4c42fa591bb9dea1f5d37e7f5019942
SigmaGrindset <antoniobnoni@gmail.com>
2023-03-16 20:57:13 +0100

    ne radi bas

--- test/cypress/integration/teams.cy.js
+++ test/cypress/integration/teams.cy.js
@@ -10,24 +10,13 @@ describe("teams page", function () {
         $res.each(teamViewIndex => {
           const teamView = $res[teamViewIndex];
           const teamViewId = teamView.href.split("/").slice(-1)[0];
-          const teamViewName = teamView.innerText.replace(/\u00a0/g, '').replace("&nbsp", "").replace("&nbsp;", "").replace("\n", "");
+          const teamViewName = teamView.innerText.replaceAll(/\u00a0/g, '').replaceAll(" ", "");
 
+          console.log(teamViewName);
 
-          let teamViewNameChars = "";
-          for (let char of teamViewName.replace(/\u00a0/g, '').replace("&nbsp", "").replace("&nbsp;", "").replace("\n", "").replace(" ", "")) {
-            teamViewNameChars = teamViewNameChars.concat(char);
-          }
-          let teamDBChars = "";
-          for (let char of teamsDB[0].name.replace(/\u00a0/g, '').replace("&nbsp", "").replace("&nbsp;", "").replace("\n", "")) {
-            teamDBChars = teamDBChars.concat(char);
-          }
-
-          console.log(teamViewNameChars, teamDBChars);
-          console.log(teamViewNameChars == teamDBChars);
-
-
-          for (let teamDB in teamsDB) {
-            if (teamDB.name == teamViewName && teamDB._id == teamViewId) {
+          let valid = false
+          for (let teamDB of teamsDB) {
+            if (teamDB.name.replaceAll(/\u00a0/g, '').replaceAll(" ", "") == teamViewName && teamDB._id == teamViewId) {
               valid = true;
 
               break;
