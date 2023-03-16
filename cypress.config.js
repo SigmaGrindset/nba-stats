1af1a7abedd93df0812d8ede4e8cc7ab830462d8
SigmaGrindset <antoniobnoni@gmail.com>
2023-03-16 20:46:00 +0100

    USPOREDBA IMENA TIMOVA RADI ALI JE UGLY

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,6 @@ module.exports = defineConfig({
   screenshotsFolder: 'test/cypress/screenshots',
   videosFolder: 'test/cypress/videos',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./test/cypress/plugins/index.js')(on, config)
     },
